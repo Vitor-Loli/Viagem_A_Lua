d856e09cf98150ad2e3db356edfe5211d72a3140
vitor.cagliari <vgabrielloli221@gmail.com>
2025-11-21 20:52:16 -0300

    Criado classes

---
+++ src/main/java/com/example/Nave.java
@@ -0,0 +1,39 @@
+package com.example;
+
+public abstract class Nave {
+    private String id;
+    private String modelo;
+    private boolean disponivel;
+
+    public Nave(){
+
+    }
+
+    public Nave(String id, String modelo, boolean disponivel) {
+        this.id = id;
+        this.modelo = modelo;
+        this.disponivel = disponivel;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public boolean getDisponivel() {
+        return disponivel;
+    }
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+    public String toString() {
+        return "[" + id + "] " + this.modelo + "\n disponivel" + (this.disponivel ? "DISPONIVEL" : "INDISPONIVEL") ;
+    }
+}
